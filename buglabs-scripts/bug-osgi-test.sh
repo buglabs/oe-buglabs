a65202659e78622609ddf287651bbc8f3d8d2bb6
Ken Gilmer <kgilmer@gmail.com>
2011-05-11 16:54:40 +0900

    oe-buglabs: add report output dir for osgi test runner.
    Change-Id: I23ec5dcee787fc54ca827c877f1aa8bf9a6009de

--- buglabs-scripts/bug-osgi-test.sh
+++ buglabs-scripts/bug-osgi-test.sh
@@ -4,6 +4,8 @@
 # The propose of the script is to execute tests against code contained in the depoy/sysroots output.
 # It is designed to be called from within and without the CI system.
 
+set -e
+
 if [ -z $WORKSPACE ]; then
 	echo "Setting WORKSPACE TO `pwd`"
 	WORKSPACE=`pwd`
@@ -19,16 +21,16 @@ DEPS_DIR=$TEST_ROOT/subjects
 
 if [ ! -d $DEPS_DIR ]; then
 	mkdir $DEPS_DIR
-else 
-	rm -Rf $DEPS_DIR
-	mkdir $DEPS_DIR
+#else 
+#	rm -Rf $DEPS_DIR
+#	mkdir $DEPS_DIR
 fi
 
 echo "Copying oe-built jars from $JAVA_OE_BUILD_DIR"
-cp $JAVA_OE_BUILD_DIR/* $DEPS_DIR
+#cp $JAVA_OE_BUILD_DIR/* $DEPS_DIR
 
-if [ ! -f $DEPS_DIR/junit-dep-4.9b2.jar ]; then
-	wget --no-check-certificate -P $DEPS_DIR https://github.com/downloads/KentBeck/junit/junit-dep-4.9b2.jar
+if [ ! -f $DEPS_DIR/junit-osgi-4.9b2.jar ]; then
+	wget --no-check-certificate -P $DEPS_DIR https://github.com/downloads/buglabs/bug-osgi/junit-osgi-4.9b2.jar
 fi 
 
 if [ ! -d $TEST_ROOT ]; then
@@ -48,15 +50,24 @@ fi
 if [ ! -d $BUILD_TOOLS ]; then
 	echo "com.buglabs.osgi.build is not at $BUILD_TOOLS, aborting."
 	exit 1
-else 
-	cd bug-osgi
-	git reset --hard HEAD
-	git pull
-	cd ..
+#else 
+#	cd bug-osgi
+#	git reset --hard HEAD
+#	git pull
+#	cd ..
 fi
+
+
 
 echo "Running tests com.buglabs.common.tests"
 ant -Dreport.dir=$TEST_ROOT/junit-reports -Dbase.build.dir=$BUILD_TOOLS -Dcheckout.dir=$TEST_ROOT/bug-osgi -DexternalDirectory=$DEPS_DIR -DdistDirectory=$DEPS_DIR -f $TEST_ROOT/bug-osgi/com.buglabs.common.tests/build.xml test
+
+if [ ! -f $DEPS_DIR/com.buglabs.osgi.tester.jar ]; then
+	echo "Building OSGi test runner"
+	ant -Dbase.build.dir=$BUILD_TOOLS -Dcheckout.dir=$TEST_ROOT/bug-osgi -DexternalDirectory=$DEPS_DIR -DdistDirectory=$DEPS_DIR -f $TEST_ROOT/bug-osgi/com.buglabs.osgi.tester/build.xml build.jars
+fi
+
+ant -Dreport.dir=$TEST_ROOT/junit-reports -Dcom.buglabs.osgi.tester.report.dir=$TEST_ROOT/junit-reports -Dbase.build.dir=$BUILD_TOOLS -Dcheckout.dir=$TEST_ROOT/bug-osgi -DexternalDirectory=$DEPS_DIR -DdistDirectory=$DEPS_DIR -f $TEST_ROOT/bug-osgi/com.buglabs.common.tests/build.xml test-osgi
 
 cd $WORKSPACE
 echo "Test phase complete."
